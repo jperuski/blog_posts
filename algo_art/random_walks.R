f0ffab35bf0aa7f584913983cdc1c8ce5bc40bd2
jperuski <jperuski>
2018-08-09 01:05:53 -0400

    final algo art update

--- algo_art/random_walks.R
+++ algo_art/random_walks.R
@@ -14,17 +14,42 @@ walk_algo <- function(n, d = 2) {
 ## plot in stripped down base graphics
 algo_plot <- function(n, col_i = "black") plot(walk_algo(n), type = "l", xlab = NA, ylab = NA, tcl = 0, col = col_i)
 
-## consistent step size, variable step count
-set.seed(1234)
-par(mfrow = c(3,3), xaxt = "n", yaxt = "n", bty = "n", mar=c(0.5,0.5,0.5,0.5))
-for (i in 1:9) algo_plot(n = sample(c(30,40,50,60,70), 1))
-par(mfrow = c(1,1))
-
 ## binding walks into a grid
 set.seed(12345)
-par(mfrow = c(3,3), xaxt = "n", yaxt = "n", bty = "n", mar=c(0.5,0.5,0.5,0.5))
-for (i in 1:9) algo_plot(n = 500)
+par(mfrow = c(1,6), xaxt = "n", yaxt = "n", bty = "n", mar=c(0.5,0.5,0.5,0.5))
+for (i in 1:6) algo_plot(n = 500)
 par(mfrow = c(1,1))
+
+## using process two
+process <- function(d) rnorm(d, mean = 0, sd = 0.5)
+
+set.seed(12345)
+par(mfrow = c(1,6), xaxt = "n", yaxt = "n", bty = "n", bg = "white", mar=c(0.5,0.5,0.5,0.5))
+for (i in 1:6) algo_plot(n = 500, col = "black")
+par(mfrow = c(1,1))
+
+## high contrast
+colors <- c()
+for(ii in seq(10, 90, by = 10)) colors <- c(colors, paste0("grey",ii))
+set.seed(12345)
+par(mfrow = c(3,3), xaxt = "n", yaxt = "n", bty = "n", bg = "black", mar=c(0.5,0.5,0.5,0.5))
+for (i in 1:9) algo_plot(n = 500, col_i = colors[10-i])
+par(mfrow = c(1,1))
+
+## High step counts
+set.seed(12345)
+par(mfrow = c(1,3), xaxt = "n", yaxt = "n", bty = "n", bg = "white", mar=c(0.5,0.5,0.5,0.5))
+for (i in 1:9) algo_plot(n = 15000, col = "grey50")
+par(mfrow = c(1,1))
+
+## Cover plot
+colors <- c("grey15", "grey30", "grey45", "grey60", "grey70", "grey80")
+set.seed(345)
+par(mfrow = c(2,6), xaxt = "n", yaxt = "n", bty = "n", mar=c(0.5,0.5,0.5,0.5))
+for (i in 1:12) algo_plot(n = 500, col_i = colors[(i-1)%%6+1])
+par(mfrow = c(1,1))
+
+############################## Unutilized noise overlay ############################
 
 ## plotting utility with noise overlays
 algo_plot2 <- function(n, d) {
@@ -36,22 +61,8 @@ algo_plot2 <- function(n, d) {
   lines(walk_mat, col = "black")
 }
 
-process <- function(d) rnorm(d, mean = 0, sd = 0.5)
-set.seed(12345)
-par(mfrow = c(3,3), xaxt = "n", yaxt = "n", bty = "n", mar=c(0.5,0.5,0.5,0.5))
-for (i in 1:9) algo_plot(n = 15000, col = "grey50")
-par(mfrow = c(1,1))
-
 ## binding noise overlay plots
 set.seed(1234)
 par(mfrow = c(3,3), xaxt = "n", yaxt = "n", bty = "n", mar=c(0.5,0.5,0.5,0.5))
 for (i in 1:9) algo_plot2(n = 500, d = 2)
 par(mfrow = c(1,1))
-
-## Cover plot
-colors <- c("grey15", "grey30", "grey45", "grey60", "grey70", "grey80")
-set.seed(345)
-par(mfrow = c(2,6), xaxt = "n", yaxt = "n", bty = "n", mar=c(0.5,0.5,0.5,0.5))
-for (i in 1:12) algo_plot(n = 500, col_i = colors[(i-1)%%6+1])
-par(mfrow = c(1,1))
-
